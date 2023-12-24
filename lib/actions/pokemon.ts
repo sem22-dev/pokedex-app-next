191bca16b56c9426e855998c45732ef6e9a0168f
sem22-dev <thotjj22@gmail.com>
2023-12-24 15:13:35 +0530

    pokemon limit 659

--- lib/actions/pokemon.ts
+++ lib/actions/pokemon.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk, createAction, createSlice } from '@reduxjs/toolkit';
 
 
 const fetchPokemonListAPI = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025');
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=649');
   const data = await res.json();
   return data.results;
 };
